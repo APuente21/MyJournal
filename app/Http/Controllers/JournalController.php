dbf38a7b6822a07886b381ec9cf28b2de952ed00
Andres Puente <andres.puente012@gmail.com>
2017-12-01 15:10:10 -0500

    updated controller and model

--- app/Http/Controllers/JournalController.php
+++ app/Http/Controllers/JournalController.php
@@ -29,21 +29,21 @@ class JournalController extends Controller {
                 'title' => 'required',
                 'tags' => 'required'
             ]);
-            $tags = explode(" ", $request->input('tags'));
+            $tagsInForm = explode(" ", $request->input('tags'));
             
             $journalEntry= new Post();
             $journalEntry->title = $request->input('title');
             $journalEntry->post = $request->input('journal-entry');
             $journalEntry->save();
             
-            for ($x = 0; $x<count($tags); $x++){
-                $result = (Tag::where('tag', '=', $tags[$x])->get())->toArray();
-                if(count($result) == 0){
+            for ($x = 0; $x<count($tagsInForm); $x++){
+                $result = Tag::where('tag', '=', $tagsInForms[$x])->get();
+                if(count($result->toArray()) == 0){
                     $tag= new Tag();
                     $tag->tag = $tags[$x];  
                     $tag->save();
                     $tag->posts()->save($journalEntry);
-                }               
+                }           
             }
 
            return redirect('/');  
@@ -54,22 +54,53 @@ class JournalController extends Controller {
                 'title' => 'required',
                 'tags' => 'required'
             ]);
-            $result = App\Post::where('created_at', '=', $_POST['date'])->first();
+            $tagsInForm = explode(" ", $request->input('tags'));
+            
+            $result = Post::with('tags')->where('created_at', '=', $_POST['date'])->first();
             $result->title = $request->input('title');
             $result->post = $request->input('journal-entry');
+            $result->save(); 
             
+            $tagsInPost = $result->tags;
             
+            //query tag, if not in database then add to tag and relation table in db
+            for ($x = 0; $x<count($tagsInForm); $x++){
+                $tagInDB = Tag::where('tag', '=', $tagsInForm[$x])->get();
+
+                //if it's a new tag, create it, save it, then link it to post
+                if($tagInDB->isEmpty()){
+                    $tag= new Tag();
+                    $tag->tag = $tagsInForm[$x];  
+                    $tag->save();
+                    $tag->posts()->save($result);
+                } else {
+                    //check if the tag is alrady in the dabatase, but has not been mapped to this post
+                    $test = false;
+                    foreach ($tagsInPost as $myTag){
+                        if (strcmp($myTag->tag,$tagInDB[0]->tag) == 0){
+                            $test = true;
+                            break;
+                        }
+                    }
+                    //if it hasn't been mapped, then map the tag to the post
+                    if(!$test) {
+                        $result->tags()->save($tagInDB[0]);
+                    }
+                }        
+            }
             
-            #implement tag update
-           ## $result->tags()->sync($request->input('tags'));
-        
-            
-            $result->save();
+            //loop through all the tags in the post, if any of the posts have been removed, then detach them in the db
+            for ($y = 0; $y < count($tagsInPost); $y++){
+                if(!(in_array($tagsInPost[$y]->tag,$tagsInForm))){
+                    $tagsInPost[$y]->posts()->detach($result->id);
+                }
+            }
             return redirect('/');  
         }
         
         else if (isset($_POST['delete_button'])) {
             $result = Post::where('created_at', '=', $_POST['date'])->delete();
+            
             return redirect('/'); 
         }
         
@@ -86,11 +117,10 @@ class JournalController extends Controller {
         $tagsForForm = '';
         for ($x = 0; $x < count($book[0]['tags']); $x++){
             if ($x == count($book[0]['tags']) - 1)
-                $tagsForForm .= ' '. $book[0]['tags'][$x]['tag'];
-            else
                 $tagsForForm .= $book[0]['tags'][$x]['tag'];
+            else
+                $tagsForForm .= $book[0]['tags'][$x]['tag']. ' ';
         }
-        
         return view('edit')->with([
             'data' => $results,
             'tags' => $tags,
@@ -102,7 +132,6 @@ class JournalController extends Controller {
     public function search($id){
         $results = Post::orderBy('created_at')->get();
         $tags = Tag::all();
-        
         $posts = (Tag::with('posts')->where('tag','=',$id)->get()->toArray())[0]['posts'];
         return view('search')->with([
             'search' =>  $id,
--- app/Post.php
+++ app/Post.php
@@ -8,4 +8,14 @@ class Post extends Model {
     public function tags() {
         return $this->belongsToMany('App\Tag')->withTimestamps();
     }
+    
+    public static function getAllTags(){
+        $tags = Tag::orderBy('id')->get();
+        $tagsArrayByID = [];
+        
+        foreach ($tags as $tag) {
+            $tagsArrayByID[$tag['id']] = $tag->tag;
+        }
+        return $tagsForCheckboxes;
+    }
 }
